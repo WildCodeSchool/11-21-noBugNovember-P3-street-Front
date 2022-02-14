bfc29d4ccc1608621efaca34f572ac793d1a2b6b
Yannick COUSIN <94236356+yannick-cousin@users.noreply.github.com>
2022-02-14 15:13:10 +0100

    Create Routes on Front

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './screens/Home';
+import Navbar from './components/Navbar';
+import Project from './screens/Project';
+import Users from './screens/Users';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<Navbar />
+			</header>
+			<Routes>
+				<Route>path='/' element={<Home />}</Route>
+				<Route>path='/projets' element={<Project />}</Route>
+				<Route>path='/talents' element={<Users />} </Route>
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
