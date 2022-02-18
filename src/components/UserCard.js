2b28691267b82668558bd87693585ea6e47e36b3
Yannick COUSIN <94236356+yannick-cousin@users.noreply.github.com>
2022-02-18 09:28:28 +0100

    Update

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,53 +1,33 @@
-import "./UserCard.css";
-import avatar from "../assets/avatar.jpg";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-const ProjectCard = () => {
-  return (
-    <div className="userContainer">
-      <div className="cardHeader">
-        <div className="catName">
-          <div className="catContainer">
-            <h1>PEINTRE</h1>
-          </div>
-          <div className="name">
-            <h3>Romain Marsaleix</h3>
-          </div>
-        </div>
-        <div className="avatarContainer">
-          <img src={avatar} alt="" className="avatar" />
-        </div>
-      </div>
-      <div className="userInfo">
-        {/* <p>Romain Marsaleix, 31ans</p> */}
+import './UserCard.css';
+import avatar from '../assets/avatar.jpg';
+//import { FontAwesomeIcon } from '@fontawesome/react-fontawesome';
 
-        <p>
-          <img
-            src="https://img.icons8.com/cotton/20/000000/worldwide-location--v1.png"
-            className="locIcon"
-          />
-          Nice
-        </p>
-        <p>
-          <img
-            src="https://img.icons8.com/external-kiranshastry-lineal-color-kiranshastry/20/000000/external-calendar-logistic-delivery-kiranshastry-lineal-color-kiranshastry-1.png"
-            className="locIcon"
-          />
-          Disponible dès janvier 2022
-        </p>
-        <div className="descriptionUser">
-          <p>
-            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure eos
-            sequi, facere magnam dolor molestiae in minima illo. Obcaecati
-            dolorum numquam, consectetur dolore quasi non vitae facere veritatis
-            nesciunt repellat!"
-          </p>
-          <div>
-            <button className="buttonUser">CONTACTER</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+const ProjectCard = () => {
+	return (
+		<div className="userContainer">
+			<div className="head">
+				<div className="avatar">
+					<img src={avatar} />
+				</div>
+				<div className="rightside">
+					<div className="job">Danseuse étoile</div>
+					<div className="pseudo">Romain - Julien V2</div>
+				</div>
+			</div>
+			<div className="desc">
+				Bonjour, je suis danseuse étoile avec tutu et tout le bazar et je suis
+				disponible très prochainement
+			</div>
+			<div className="content">
+				<div className="localisation">
+					<i class="fa-solid fa-location-dot"></i> Nice - France
+				</div>
+				<div className="dispo">
+					<i className="fa-solid fa-calendar-day" /> 01/03/2022
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ProjectCard;
--- src/screens/Project.js
+++ src/screens/Project.js
@@ -1,16 +1,14 @@
-import ProjectCard from "../components/ProjectCard";
-import UserCard from "../components/UserCard";
+import ProjectCard from '../components/ProjectCard';
+import UserCard from '../components/UserCard';
 const Project = () => {
-  return (
-    <div>
-      <div className="navbar">PROJECTS</div>
-      <div>
-        <ProjectCard />
-
-        <UserCard />
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className="navbar">PROJECTS</div>
+			<div>
+				<UserCard />
+			</div>
+		</div>
+	);
 };
 
 export default Project;
