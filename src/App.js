f1e2c35694ffd99145ea69d5d1109122e9855ebd
Mickaseg <mickael_seguin@hotmail.fr>
2022-03-04 09:57:09 +0100

    merge

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import Connexion from "./screens/Connexion";
-<<<<<<< HEAD
 import Admin from "./screens/Admin";
-=======
->>>>>>> dev
 import Home from "./screens/Home";
 import Navbar from "./components/Navbar";
 import Project from "./screens/Project";
@@ -10,15 +7,10 @@ import Users from "./screens/Users";
 import CreateProject from "./components/CreateProject";
 import UserDetail from "./screens/UserDetail";
 import ProjectDetails from "./screens/ProjectDetails";
-<<<<<<< HEAD
 import AdminGestionUsers from "./screens/AdminGestionUsers";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-=======
-import { Routes, Route } from "react-router-dom";
-import "./App.css";
 import Footer from "./components/Footer";
->>>>>>> dev
 
 function App() {
   return (
@@ -26,26 +18,16 @@ function App() {
       <header className="App-header">
         <Navbar />
       </header>
-<<<<<<< HEAD
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/add_projects" element={<CreateProject />} />
         <Route path="/admin" element={<Admin />} />
-=======
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/add_projects" element={<CreateProject />} />
->>>>>>> dev
         <Route path="/connexion" element={<Connexion />} />
         <Route path="/projets" element={<Project />} />
         <Route path="/talents" element={<Users />} />
         <Route path="/projets/:id" element={<ProjectDetails />} />
         <Route path="/talents/:id" element={<UserDetail />} />
-<<<<<<< HEAD
         <Route path="/admin/users" element={<AdminGestionUsers />} />
-=======
->>>>>>> dev
       </Routes>
     </div>
   );
