f899ece42dcbc57d26d5b1abe90625c69bb8122a
Yannick COUSIN <94236356+yannick-cousin@users.noreply.github.com>
2022-03-04 09:56:46 +0100

    Update

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,26 +4,28 @@ const Dashboard = (props) => {
 	return (
 		<div className="dashboard">
 			<div className="title">Dashboard</div>
-			<div className="dashusers">
-				<div className="titleuser">Utilisateurs</div>
-				<div className="contentusers">
-					Nombre d'utilisateurs enregistrés : {props.users.length}
-					<br />
-					Nombre d'utilisateurs bloqués : {props.usersBlocked()}
+			<div className="infos">
+				<div className="dashusers">
+					<div className="titleuser">Utilisateurs</div>
+					<div className="contentusers">
+						Nombre d'utilisateurs enregistrés : {props.users.length}
+						<br />
+						Nombre d'utilisateurs bloqués : {props.usersBlocked()}
+					</div>
 				</div>
-			</div>
-			<div className="dashprojects">
-				<div className="titleproject">Projets</div>
-				<div className="contentusers">
-					Nombre de projets enregistrés : {props.projects.length}
-					<br />
-					Nombre de projets bloqués : {props.projectsBlocked()}
-					<br />
-					Nombre de projets en recherche d'équipiers : {props.projectsMate()}
-					<br />
-					Nombre de projets avec équipe complete : {props.projectsCompleted()}
-					<br />
-					Nombre de projets terminés : {props.projectsFinish()}
+				<div className="dashprojects">
+					<div className="titleproject">Projets</div>
+					<div className="contentusers">
+						Nombre de projets enregistrés : {props.projects.length}
+						<br />
+						Nombre de projets bloqués : {props.projectsBlocked()}
+						<br />
+						Nombre de projets en recherche d'équipiers : {props.projectsMate()}
+						<br />
+						Nombre de projets avec équipe complete : {props.projectsCompleted()}
+						<br />
+						Nombre de projets terminés : {props.projectsFinish()}
+					</div>
 				</div>
 			</div>
 		</div>
